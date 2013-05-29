c9c0a5ab204399659c0ce82c5d1b82c7f58918a2
Carlos Sancho Ramirez <carlos.sancho.ramirez@gmail.com>
2013-05-29 23:02:02 +0200

    Added Packed values structure up to 8 values.

--- lib/src/PackedValues.hpp
+++ lib/src/PackedValues.hpp
@@ -92,6 +92,7 @@ template<class type1, class type2, class type3,
 struct Packed3Values : public Packed2Values<type1, type2, ALL_NEEDED_BITS> {
 
     typedef Packed2Values<type1, type2, ALL_NEEDED_BITS> parentType;
+    typedef parentType baseType;
 
     enum
     {
@@ -111,6 +112,159 @@ struct Packed3Values : public Packed2Values<type1, type2, ALL_NEEDED_BITS> {
     type3 getValueForType3()
     {
         return ((parentType::mPacked & TYPE3_BIT_MASK) >> TYPE3_BIT_BASE) + type3::MIN_VALUE;
+    }
+};
+
+template<class type1, class type2, class type3, class type4,
+		int ALL_NEEDED_BITS = type1::NEEDED_BITS + type2::NEEDED_BITS +
+        type3::NEEDED_BITS + type4::NEEDED_BITS>
+struct Packed4Values : public Packed3Values<type1, type2, type3, ALL_NEEDED_BITS> {
+
+    typedef Packed3Values<type1, type2, type3, ALL_NEEDED_BITS> parentType;
+    typedef typename parentType::baseType baseType;
+
+    enum
+    {
+        TYPE4_BIT_BASE = parentType::TYPE4_BIT_BASE,
+        TYPE5_BIT_BASE = TYPE4_BIT_BASE + type4::NEEDED_BITS,
+        TYPE4_BIT_MASK = MathPower<2,TYPE5_BIT_BASE>::value - MathPower<2,TYPE4_BIT_BASE>::value
+    };
+
+    public:
+    void setValueForType4(type4 value)
+    {
+        typename parentType::packedType converted = value;
+        parentType::mPacked &= ~TYPE4_BIT_MASK;
+        parentType::mPacked |= (converted - type4::MIN_VALUE) << TYPE4_BIT_BASE;
+    }
+
+    type4 getValueForType4()
+    {
+        return ((parentType::mPacked & TYPE4_BIT_MASK) >> TYPE4_BIT_BASE) + type4::MIN_VALUE;
+    }
+};
+
+template<class type1, class type2, class type3, class type4, class type5,
+		int ALL_NEEDED_BITS = type1::NEEDED_BITS + type2::NEEDED_BITS +
+        type3::NEEDED_BITS + type4::NEEDED_BITS + type5::NEEDED_BITS>
+struct Packed5Values : public Packed4Values<type1, type2, type3, type4, ALL_NEEDED_BITS> {
+
+    typedef Packed4Values<type1, type2, type3, type4, ALL_NEEDED_BITS> parentType;
+    typedef typename parentType::baseType baseType;
+
+    enum
+    {
+        TYPE5_BIT_BASE = parentType::TYPE5_BIT_BASE,
+        TYPE6_BIT_BASE = TYPE5_BIT_BASE + type5::NEEDED_BITS,
+        TYPE5_BIT_MASK = MathPower<2,TYPE6_BIT_BASE>::value - MathPower<2,TYPE5_BIT_BASE>::value
+    };
+
+    public:
+    void setValueForType5(type5 value)
+    {
+        typename parentType::packedType converted = value;
+        parentType::mPacked &= ~TYPE5_BIT_MASK;
+        parentType::mPacked |= (converted - type5::MIN_VALUE) << TYPE5_BIT_BASE;
+    }
+
+    type5 getValueForType5()
+    {
+        return ((parentType::mPacked & TYPE5_BIT_MASK) >> TYPE5_BIT_BASE) + type5::MIN_VALUE;
+    }
+};
+
+template<class type1, class type2, class type3, class type4, class type5,
+        class type6, int ALL_NEEDED_BITS = type1::NEEDED_BITS +
+        type2::NEEDED_BITS + type3::NEEDED_BITS + type4::NEEDED_BITS +
+        type5::NEEDED_BITS + type6::NEEDED_BITS>
+struct Packed6Values : public Packed5Values<type1, type2, type3, type4, type5, ALL_NEEDED_BITS> {
+
+    typedef Packed5Values<type1, type2, type3, type4, type5, ALL_NEEDED_BITS> parentType;
+    typedef typename parentType::baseType baseType;
+
+    enum
+    {
+        TYPE6_BIT_BASE = parentType::TYPE6_BIT_BASE,
+        TYPE7_BIT_BASE = TYPE6_BIT_BASE + type6::NEEDED_BITS,
+        TYPE6_BIT_MASK = MathPower<2,TYPE7_BIT_BASE>::value - MathPower<2,TYPE6_BIT_BASE>::value
+    };
+
+    public:
+    void setValueForType6(type6 value)
+    {
+        typename parentType::packedType converted = value;
+        baseType::mPacked &= ~TYPE6_BIT_MASK;
+        baseType::mPacked |= (converted - type6::MIN_VALUE) << TYPE6_BIT_BASE;
+    }
+
+    type6 getValueForType6()
+    {
+        return ((baseType::mPacked & TYPE6_BIT_MASK) >> TYPE6_BIT_BASE) + type6::MIN_VALUE;
+    }
+};
+
+template<class type1, class type2, class type3, class type4, class type5,
+        class type6, class type7, int ALL_NEEDED_BITS = type1::NEEDED_BITS +
+        type2::NEEDED_BITS + type3::NEEDED_BITS + type4::NEEDED_BITS +
+        type5::NEEDED_BITS + type6::NEEDED_BITS + type7::NEEDED_BITS>
+struct Packed7Values : public Packed6Values<type1, type2, type3, type4, type5,
+        type6, ALL_NEEDED_BITS> {
+
+    typedef Packed6Values<type1, type2, type3, type4, type5, type6,
+            ALL_NEEDED_BITS> parentType;
+    typedef typename parentType::baseType baseType;
+
+    enum
+    {
+        TYPE7_BIT_BASE = parentType::TYPE7_BIT_BASE,
+        TYPE8_BIT_BASE = TYPE7_BIT_BASE + type7::NEEDED_BITS,
+        TYPE7_BIT_MASK = MathPower<2,TYPE8_BIT_BASE>::value - MathPower<2,TYPE7_BIT_BASE>::value
+    };
+
+    public:
+    void setValueForType7(type7 value)
+    {
+        typename parentType::packedType converted = value;
+        baseType::mPacked &= ~TYPE7_BIT_MASK;
+        baseType::mPacked |= (converted - type7::MIN_VALUE) << TYPE7_BIT_BASE;
+    }
+
+    type7 getValueForType7()
+    {
+        return ((baseType::mPacked & TYPE7_BIT_MASK) >> TYPE7_BIT_BASE) + type7::MIN_VALUE;
+    }
+};
+
+template<class type1, class type2, class type3, class type4,
+        class type5, class type6, class type7, class type8,
+        int ALL_NEEDED_BITS = type1::NEEDED_BITS + type2::NEEDED_BITS +
+        type3::NEEDED_BITS + type4::NEEDED_BITS + type5::NEEDED_BITS +
+        type6::NEEDED_BITS + type7::NEEDED_BITS + type8::NEEDED_BITS>
+struct Packed8Values : public Packed7Values<type1, type2, type3, type4, type5,
+        type6, type7, ALL_NEEDED_BITS> {
+
+    typedef Packed7Values<type1, type2, type3, type4, type5, type6, type7,
+            ALL_NEEDED_BITS> parentType;
+    typedef typename parentType::baseType baseType;
+
+    enum
+    {
+        TYPE8_BIT_BASE = parentType::TYPE8_BIT_BASE,
+        TYPE9_BIT_BASE = TYPE8_BIT_BASE + type8::NEEDED_BITS,
+        TYPE8_BIT_MASK = MathPower<2,TYPE9_BIT_BASE>::value - MathPower<2,TYPE8_BIT_BASE>::value
+    };
+
+    public:
+    void setValueForType8(type8 value)
+    {
+        typename baseType::packedType converted = value;
+        baseType::mPacked &= ~TYPE8_BIT_MASK;
+        baseType::mPacked |= (converted - type8::MIN_VALUE) << TYPE8_BIT_BASE;
+    }
+
+    type8 getValueForType8()
+    {
+        return ((baseType::mPacked & TYPE8_BIT_MASK) >> TYPE8_BIT_BASE) + type8::MIN_VALUE;
     }
 };
 
--- sample/src/packed_values.cpp
+++ sample/src/packed_values.cpp
@@ -4,7 +4,8 @@
 
 typedef Packed2Values<StaticIntegerRange<0,9>, StaticIntegerRange<4,5> > packed2_t;
 typedef Packed3Values<StaticIntegerRange<0,9>, StaticIntegerRange<4,5>, StaticIntegerRange<-5,5> > packed3_t;
-
+typedef StaticIntegerRange<0,1> bool_t;
+typedef Packed8Values<bool_t, bool_t, bool_t, bool_t, bool_t, bool_t, bool_t, bool_t> flags_t;
 using namespace std;
 
 int main(int argc, char *argv[])
@@ -24,6 +25,23 @@ int main(int argc, char *argv[])
 
 	cout << "Product is " << (pack3.getValueForType1() * pack3.getValueForType2()
 			* pack3.getValueForType3()) << endl;
+
+    // Pack of 8 booleans
+	flags_t flags;
+	flags.setValueForType1(0);
+	flags.setValueForType2(1);
+	flags.setValueForType3(0);
+	flags.setValueForType4(1);
+	flags.setValueForType5(0);
+	flags.setValueForType6(1);
+	flags.setValueForType7(1);
+	flags.setValueForType8(1);
+
+    const int ones = flags.getValueForType1() + flags.getValueForType2() +
+            flags.getValueForType3() + flags.getValueForType4() + flags.getValueForType5() +
+            flags.getValueForType6() + flags.getValueForType7() + flags.getValueForType8();
+
+	cout << "There is " << ones << " flags set" << endl;
 
     return 0;
 }
